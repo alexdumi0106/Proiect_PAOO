4f1448541ee5c5aaa9dfcedb28a02b2cfd84f7fb
alexdumi0106 <alexdumi0106@gmail.com>
2025-12-02 20:45:26 +0200

    Initial commit for tema 3

---
+++ build/CMakeFiles/tema3_core.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tema3_core.dir/src/Account.cpp.o"
+  "CMakeFiles/tema3_core.dir/src/Account.cpp.o.d"
+  "bin/libtema3_core.a"
+  "bin/libtema3_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tema3_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
